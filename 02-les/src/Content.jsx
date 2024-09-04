b75b06d77f5471473e63bab98b471c91cd157d74
dimasolyshko <99644055+dimasolyshko@users.noreply.github.com>
2024-09-04 16:47:49 +0300

    useState
    Use hook useState

--- 02-les/src/Content.jsx
+++ 02-les/src/Content.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Content = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Hello world",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Hello Nikke",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Hello Tikke",
+    }
+  ])
+  console.log(items);
   return (
     <main>
         Project
